f8a55ede8a052e55bc322eaa2d24d1d02c190eaf
lokulin <lauchlin@lauchlin.com>
2015-04-06 03:04:49 +1000

    Only turn when placed

--- src/main/scala/com/lauchlin/toyrobot/robot.scala
+++ src/main/scala/com/lauchlin/toyrobot/robot.scala
@@ -8,18 +8,16 @@ class Robot(var table: Table) {
   val Directions=Array("EAST","NORTH","WEST","SOUTH")
 
   def place(nx: Long, ny: Long, nDirection: String) =
-    if ( Directions.contains(nDirection) && table.contains(Point(nx,ny)) ){
+    if (Directions.contains(nDirection) && table.contains(Point(nx,ny))) {
       loc = Point(nx, ny)
       facing = Directions.indexOf(nDirection)
     }
 
-  def move() = if (facing != -1 && table.contains(lookingAt)) loc = lookingAt
-
-  def left() = facing = (facing+1) % Directions.length 
-
-  def right() = facing = floorMod(facing-1, Directions.length)
-
-  def report = if (facing != -1) println(loc.x+","+loc.y+","+Directions(facing))
+  def move()  = if (placed && table.contains(lookingAt)) loc = lookingAt
+  def left()  = if (placed) facing = (facing+1) % Directions.length
+  def right() = if (placed) facing = floorMod(facing-1, Directions.length)
+  def report  = if (placed) println(loc.x+","+loc.y+","+Directions(facing))
 
   private def lookingAt = loc+Point(round(cos(PI*facing/2.0)),round(sin(PI*facing/2.0)))
+  private def placed = facing != -1
 }

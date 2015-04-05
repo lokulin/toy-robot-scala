9498752c4d9f5835f14d308b207e70932be58d5e
lokulin <lauchlin@lauchlin.com>
2015-04-06 02:15:14 +1000

    noop on no command match

--- src/main/scala/com/lauchlin/toyrobot/toyrobot.scala
+++ src/main/scala/com/lauchlin/toyrobot/toyrobot.scala
@@ -11,14 +11,15 @@ object ToyRobot extends App {
       case "LEFT" => robot.left()
       case "RIGHT" => robot.right()
       case Place(x, y, direction) => robot.place(x.toInt, y.toInt, direction)
-      case _ => println("Skipping: " + command)
+      case _ => ()
     }
 
     var robot=new Robot(new Table(new Point(0,0), new Point(4,4)))
+    
     if ( args.length == 1 ) {
       Source.fromFile(args(0)).getLines.foreach(execute)
     } else {
-      Console.err.println("usage: ")
+      Console.err.println("usage: ./run.sh <input.txt>")
       System.exit(1) 
     }
 }

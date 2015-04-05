d22b64a612e658bb7b09379dd190868a34d6cdb9
lokulin <lauchlin@lauchlin.com>
2015-04-06 02:27:30 +1000

    compactify the codes

--- src/main/scala/com/lauchlin/toyrobot/robot.scala
+++ src/main/scala/com/lauchlin/toyrobot/robot.scala
@@ -7,34 +7,21 @@ class Robot(var table: Table) {
   var facing = -1
   val Directions=Array("EAST","NORTH","WEST","SOUTH")
 
-  def place(nx: Long, ny: Long, nDirection: String) {
+  def place(nx: Long, ny: Long, nDirection: String) =
     if ( Directions.contains(nDirection) && table.contains(Point(nx,ny)) ){
       loc = Point(nx, ny)
       facing = Directions.indexOf(nDirection)
     }
-  }
 
-  def move() {
-    if ( facing != -1 && table.contains(lookingAt) )
-      loc = lookingAt
-  }
+  def move() = if ( facing != -1 && table.contains(lookingAt) )
+    loc = lookingAt
 
-  def left() {
-    facing = (facing+1) % Directions.length
-  }
+  def left() = facing = (facing+1) % Directions.length 
 
-  def right() {
-    facing = floorMod(facing-1, Directions.length)
-  }
+  def right() = facing = floorMod(facing-1, Directions.length)
 
-  def report {
-    if ( facing != -1 )
-      println(loc.x + "," + loc.y + "," + Directions(facing))
-  }
-
-  def execute(command: String) {
-    println("I'm goin to " + command)
-  }
+  def report = if ( facing != -1 )
+    println(loc.x + "," + loc.y + "," + Directions(facing))
 
   private def lookingAt = loc+Point(round(cos(facing)),round(sin(facing)))
 }

f926425e789ee5468944f6238ea24f707c0157ee
lokulin <lauchlin@lauchlin.com>
2015-04-06 17:12:25 +1000

    Make it even harder to read!

--- src/main/scala/com/lauchlin/toyrobot/toyrobot.scala
+++ src/main/scala/com/lauchlin/toyrobot/toyrobot.scala
@@ -19,26 +19,23 @@ object ToyRobot extends App {
       def contains(p: Point) = p >= llc && p <= urc
     }
   val Place="""^PLACE (\d),(\d),([A-Z]+)$""".r
-
-  def execute(command: String) = command match {
-    case "MOVE" => if (placed && Table.contains(lookingAt)) location = lookingAt
-    case "REPORT" => if (placed) println(location.x+","+location.y+","+Directions(facing))
-    case "LEFT" => if (placed) facing = (facing+1) % Directions.length
-    case "RIGHT" => if (placed) facing = floorMod(facing-1, Directions.length)
-    case Place(x, y, direction) => 
-      if (Directions.contains(direction) && 
-            Table.contains(Point(x.toInt,y.toInt))) {
-        location= Point(x.toInt, y.toInt)
-        facing = Directions.indexOf(direction)
-      }
-    case _ => ()
-  }
-
   def lookingAt = location+Point(round(cos(PI*facing/2.0)),round(sin(PI*facing/2.0)))
   def placed = facing != -1
 
   if ( args.length == 1 ) {
-    Source.fromFile(args(0)).getLines.foreach(execute)
+    Source.fromFile(args(0)).getLines.foreach(_ match {
+      case "MOVE" => if (placed && Table.contains(lookingAt)) location = lookingAt
+      case "REPORT" => if (placed) println(location.x+","+location.y+","+Directions(facing))
+      case "LEFT" => if (placed) facing = (facing+1) % Directions.length
+      case "RIGHT" => if (placed) facing = floorMod(facing-1, Directions.length)
+      case Place(x, y, direction) => 
+        if (Directions.contains(direction) &&
+              Table.contains(Point(x.toInt,y.toInt))) {
+          location = Point(x.toInt, y.toInt)
+          facing = Directions.indexOf(direction)
+        }
+      case _ => ()
+    })
   } else {
     Console.err.println("usage: ./run.sh <input.txt>")
     System.exit(1) 

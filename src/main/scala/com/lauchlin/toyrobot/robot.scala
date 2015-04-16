77b8373e1b51d5d3bf018a3cf0188d51657ef70b
lokulin <lauchlin@lauchlin.com>
2015-04-16 20:31:34 +1000

    Making use of foldLeft, shorter methods.

--- src/main/scala/com/lauchlin/toyrobot/robot.scala
+++ src/main/scala/com/lauchlin/toyrobot/robot.scala
@@ -2,16 +2,22 @@ package com.lauchlin.toyrobot
 
 import Math._
 
-case class Robot(val loc: Point, val facing: Double, val table: Container) {
+case class Robot(val loc: Point = Point(0,0), 
+                 val facing: Double = 0.5, 
+                 val table: Container = Stasis) {
 
   def place(x: Double, y: Double, facing: Double, table: Table) = newRobot(Point(x, y), facing, table)
-  def move  = newRobot(loc + Point(cos(PI*facing),sin(PI*facing)), facing, table) 
-  def left  = newRobot(loc, (facing+0.5) % 2, table)
-  def right = newRobot(loc, (((facing-0.5) % 2) + 2) % 2, table)
+
+  def move  = newRobot(loc = loc + Point(cos(PI*facing),sin(PI*facing))) 
+
+  def left  = newRobot(facing = (facing+0.5) % 2)
+
+  def right = newRobot(facing = (((facing-0.5) % 2) + 2) % 2)
+
   def report(reporter: (Double, Double, Double) => Unit) = 
         if(table.isInstanceOf[Table]) reporter(loc.x,loc.y,facing)
 
-  private def newRobot(loc: Point, facing: Double, table: Container) = 
+  private def newRobot(loc: Point = this.loc, facing: Double = this.facing, table: Container = this.table) = 
     if (table.isInstanceOf[Table] && table.contains(loc)) Robot(loc, facing, table)
     else this
 
--- src/main/scala/com/lauchlin/toyrobot/toyrobot.scala
+++ src/main/scala/com/lauchlin/toyrobot/toyrobot.scala
@@ -4,25 +4,27 @@ import scala.io._
 import Math.round
 
 object ToyRobot extends App {
-    val Place="""^PLACE (\d),(\d),([A-Z]+)$""".r
-    val Directions=Array("EAST","NORTH","WEST","SOUTH")
-    val table = Table(Point(0,0), Point(4,4))
-    var robot = Robot(Point(0,0), 0.0, Stasis)
-    
-  def execute(command: String) = command match {
-      case "MOVE"   =>  robot = robot.move
-      case "LEFT"   =>  robot = robot.left
-      case "RIGHT"  =>  robot = robot.right
-      case "REPORT" =>  robot.report((x: Double, y: Double, facing: Double) => 
-          println(round(x)+","+round(y)+","+Directions(round(facing.toFloat*2))))
-      case Place(x, y, direction) => 
-        if (Directions.contains(direction)) 
-          robot = robot.place(x.toDouble, y.toDouble, Directions.indexOf(direction)/2.0, table)
-      case _ => ()
-    }
+  val Place="""^PLACE (\d),(\d),([A-Z]+)$""".r
+  val Directions=Array("EAST","NORTH","WEST","SOUTH")
+  val table = Table(Point(0,0), Point(4,4))
+  
+  def execute(robot: Robot, command: String) = command match {
+    case "MOVE"   =>  robot.move
+    case "LEFT"   =>  robot.left
+    case "RIGHT"  =>  robot.right
+    case "REPORT" =>  robot.report((x: Double, y: Double, facing: Double) => 
+        println(round(x)+","+round(y)+","+Directions(round(facing.toFloat*2))))
+        robot
+    case Place(x, y, direction) => 
+      if (Directions.contains(direction)) 
+        robot.place(x.toDouble, y.toDouble, Directions.indexOf(direction)/2.0, table)
+      else
+        robot
+    case _ => robot
+  }
 
   if ( args.length == 1 ) {
-    Source.fromFile(args(0)).getLines.foreach(execute)
+    Source.fromFile(args(0)).getLines.foldLeft(Robot())((robot, command) => execute(robot, command))
   } else {
     Console.err.println("usage: ./run.sh <input.txt>")
     System.exit(1) 
